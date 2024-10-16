73f412f0e38b6d3fe5115ee731181e0f9fc57682
Jaruny Cárdenas <jarunyta1096@gmail.com>
2024-10-16 11:04:58 -0700

    formuluario: login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,10 @@
+<form action="">
+    <h3>Bienvenido</h3>
+    <h4>Ingresa su información de usuario para ingresar</h4>
+    <label for="">Correo eléctronico</label>
+    <input type="email">
+    <label for="">Contraseña</label>
+    <input type="password">
+    <input type="radio"><label for="">Recordar contraseña</label>
+    <button>Iniciar sesión</button>
+</form>
